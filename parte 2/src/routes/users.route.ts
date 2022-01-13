26591bf757a3e6d255090102709e7a648c875a36
MathFreitas <matheus.freitas_santos@outlook.com>
2022-01-12 23:05:59 -0300

    desenvolvendo a inserção de usuários

--- parte 2/src/routes/users.route.ts
+++ parte 2/src/routes/users.route.ts
@@ -21,13 +21,11 @@ usersRoute.get('/users/:uuid', async (req: Request<{ uuid: string }>, res: Respo
     res.status(StatusCodes.OK).send(user);
 });
 
-usersRoute.post('/users', (req: Request, res: Response, next: NextFunction) => {
+usersRoute.post('/users', async (req: Request, res: Response, next: NextFunction) => {
     const newUser = req.body;
-
-    console.log(req.body);
-
-    res.status(StatusCodes.CREATED).send(newUser);
-})
+    const uuid = await userRepository.create(newUser);
+    res.status(StatusCodes.CREATED).send(uuid);
+});
 
 usersRoute.put('/users/:uuid', (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
     const uuid = req.params.uuid;
