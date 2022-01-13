c0aabd8a0c48ce133e1051fc3e36a563b3e18e54
MathFreitas <matheus.freitas_santos@outlook.com>
2022-01-12 22:23:58 -0300

    criando a estrutura básica para conexão com banco de dados

--- parte 2/src/db.ts
+++ parte 2/src/db.ts
@@ -1,6 +1,6 @@
 import { Pool } from "pg";
 
-const connectionString = '';
+const connectionString = 'postgres://fopyshbm:CcXRYdkLVmBaErPEqdEefxVSHgbxUwMb@kesavan.db.elephantsql.com/fopyshbm';
 
 const db = new Pool({ connectionString });
 
